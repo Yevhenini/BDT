51556e747a75ba1c67f6115d620f685057fd4a71
Yevhenini <167741352+Yevhenini@users.noreply.github.com>
2024-04-24 10:10:51 +0200

    Add files via upload

---
+++ ul1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>CATTER</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+10+Charted&display=swap" rel="stylesheet">
+</head>
+<body >
+    <h1>CATTER</h1>
+   <?php
+    $c = 10;
+    $a = 20;
+    $text = "Hello CAT";
+    $text2 = "Hello CATER";
+    echo $a."<br>";
+    echo $c."<br>";
+    echo $text."<br>";
+    echo $text2."<br>";
+    echo "<p>America ya<br>".$text."<br>".$text2." a mali ".$c." a ".$a." jabluk<p>";
+
+     ?> 
+</body>
+</html>
