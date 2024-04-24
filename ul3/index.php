6f1ae5ccb4babaa6f01e87267c4a3bc6629bfef9
Yevhenini <167741352+Yevhenini@users.noreply.github.com>
2024-04-24 10:10:13 +0200

    Uloha 24.4 PHP

---
+++ ul3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    function martina() {
+        echo "HI HI HI HI";
+        echo "HU HU HU HU HU ";
+    }
+    function karolina() {
+        
+        return "M U  H AH  AH  HA   H";
+    }
+
+    function nezuko($a,$b){
+        return $a + $b;
+    }
+
+    martina();
+   echo karolina();  
+   echo nezuko(10,5);  
+    ?>
+</body>
+</html>
